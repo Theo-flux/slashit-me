c9b437c7bc8de4e4cac07fd65f1f9d305822ab00
wilh1490 <wilh1490@gmail.com>
2022-12-05 18:08:12 +0100

    🪝Hooks - useLocalStorage, useToast, useTabs

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,8 +1,16 @@
-import { useState } from "react";
-import { Ping, SaveLoginCredentials } from "../api/userAPI";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Ping, SaveLoginCredentials } from '../api/userAPI';
+import {
+  setAnyTab,
+  setExtraTab as setextratab,
+  setAnyAction as setanyaction,
+  setAnySuccess as setanysuccess,
+} from '../store/reducers/helper';
 
-export default function useLocalStorage() {
+export const useLocalStorage = () => {
   const [session, newSession] = useState(false);
+  const [sessionInfo, newSessionInfo] = useState('');
 
   const setSession = async (props) => {
     if (props?.userInfo && props?.token) {
@@ -21,5 +29,60 @@ export default function useLocalStorage() {
     }
   };
 
-  return [session, setSession, ping];
-}
+  useEffect(() => {
+    let storage = localStorage.getItem('userAuth');
+    if (storage) {
+      newSessionInfo(JSON.parse(storage));
+    }
+  }, []);
+
+  return { session, setSession, sessionInfo, ping };
+};
+
+export const useToast = () => {
+  const [options, setOptions] = useState({
+    backgroundColor: 'red',
+    textColor: '#fff',
+    showToast: false,
+    showIcon: false,
+    text: 'Invalid request',
+    duration: 10000,
+  });
+
+  function toast(option) {
+    setOptions({ ...options, ...option, showToast: true });
+  }
+
+  function fadeOut() {
+    setTimeout(() => {
+      setOptions({ ...options, showToast: false });
+    }, options.duration);
+  }
+
+  fadeOut();
+
+  return [options, toast];
+};
+
+export const useTabs = () => {
+  const dispatch = useDispatch();
+  const activeTab = useSelector((state) => state.helper.anyTab);
+  const extraTab = useSelector((state) => state.helper.extraTab);
+  const anyAction = useSelector((state) => state.helper.anyAction);
+  const anySuccess = useSelector((state) => state.helper.anySuccess);
+  const setActiveTab = (tab) => dispatch(setAnyTab(tab));
+  const setExtraTab = (tab) => dispatch(setextratab(tab));
+  const setAnyAction = (action) => dispatch(setanyaction(action));
+  const setAnySuccess = (success) => dispatch(setanysuccess(success));
+
+  return {
+    activeTab,
+    extraTab,
+    anyAction,
+    anySuccess,
+    setActiveTab,
+    setExtraTab,
+    setAnyAction,
+    setAnySuccess,
+  };
+};
